4a9d12f79f97faa52fd070adc5fd33bbf163e172
Svetoslav Gekov <svetoslav_gekov@abv.bg>
2018-03-13 17:27:49 +0200

    added one message back

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Demonstration test.");
 		System.out.println("hi");
+		System.out.println("Sending one message back.");
 	}
 
 }
