3fec77ee5e96df3e423cbc671e68e04f5cec14ad
Svetoslav Gekov <svetoslav_gekov@abv.bg>
2018-03-13 17:31:23 +0200

    conflict test

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -3,7 +3,7 @@ package demo;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Demonstration test.");
+		System.out.println("Demonstration asdasdtest.");
 		System.out.println("hi");
 		System.out.println("Sending one message back.");
 	}
